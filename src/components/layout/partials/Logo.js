c5b2f16abbcdd924b2e273c823206d0fbc77da0b
Rulial Space <98750732+Rulial@users.noreply.github.com>
2022-07-06 07:27:18 +0000

    Update

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -21,7 +21,7 @@ const Logo = ({
       <h1 className="m-0">
         <Link to="https://path-integral.com">
           <Image
-            src={require('./../../../assets/images/PIL08.png')}
+            src={require('./../../../assets/images/I0P_Website_White.png')}
             alt="Open"
             width={124}
             height={124} />
