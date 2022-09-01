2efe0e1d33daca65e497813aa40828b4aa2df8db
Rulial <98750732+Rulial@users.noreply.github.com>
2022-09-01 09:25:02 +0100

    update

--- src/utils/ScrollReveal.js
+++ src/utils/ScrollReveal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useImperativeHandle } from 'react';
+/*import React, { useState, useEffect, useImperativeHandle } from 'react';
 import PropTypes from 'prop-types';
 import { throttle } from 'lodash';
 
