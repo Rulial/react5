6828889d799349ebade05f0dc5e37adf95297968
Rulial <98750732+Rulial@users.noreply.github.com>
2022-09-01 08:23:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import { useLocation, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
-import ScrollReveal from './utils/ScrollReveal';
+//import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
 import { createMemoryHistory } from 'history'
 
@@ -35,7 +35,7 @@ const trackPage = page => {
 };
 
 
-
+/*
 const App = () => {
 
   const childRef = useRef();
@@ -58,6 +58,6 @@ const App = () => {
         </Switch>
       )} />
   );
-}
+} */
 
 export default App;
