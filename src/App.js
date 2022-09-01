046e9342cca21d583224c300fb098b233ec4e0dd
Rulial <98750732+Rulial@users.noreply.github.com>
2022-09-01 08:19:57 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-//import { useLocation, Switch } from 'react-router-dom';
+import { useLocation, Switch } from 'react-router-dom';
 import AppRoute from './utils/AppRoute';
 import ScrollReveal from './utils/ScrollReveal';
 import ReactGA from 'react-ga';
@@ -11,7 +11,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Router history={history}>
-      <App />
+      
     </Router>,
     div
   );
